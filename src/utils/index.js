00aa977f0a43b7121fbd58139a9f1336b94f2f6e
RezkyRizaldi <rezkyrizaldi30@gmail.com>
2022-09-23 05:23:18 +0700

    Add read slash command

--- src/utils/index.js
+++ src/utils/index.js
@@ -10,6 +10,7 @@ const applyText = require('./applyText');
 const applyTier = require('./applyTier');
 const applyVerificationLevel = require('./applyVerificationLevel');
 const getFlag = require('./getFlag');
+const getImageReadLocale = require('./getImageReadLocale');
 const getLanguage = require('./getLanguage');
 const getMessageType = require('./getMessageType');
 const groupMessageByAuthor = require('./groupMessageByAuthor');
@@ -31,6 +32,7 @@ module.exports = {
   applyTier,
   applyVerificationLevel,
   getFlag,
+  getImageReadLocale,
   getLanguage,
   getMessageType,
   groupMessageByAuthor,
