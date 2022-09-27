c1cbbafdce149c28e308872ea42ef14e927c85e8
RezkyRizaldi <rezkyrizaldi30@gmail.com>
2022-09-27 21:51:19 +0700

    Add roleUpdate event handling

---
+++ src/events/client/roleUpdate.js
@@ -0,0 +1,65 @@
+const {
+  AuditLogEvent,
+  bold,
+  EmbedBuilder,
+  Events,
+  time,
+  TimestampStyles,
+  WebhookClient,
+} = require('discord.js');
+
+module.exports = {
+  name: Events.GuildRoleUpdate,
+
+  /**
+   *
+   * @param {import('discord.js').Role} oldRole
+   * @param {import('discord.js').Role} newRole
+   */
+  async execute(oldRole, newRole) {
+    const { client, guild } = oldRole;
+
+    const RoleLogger = new WebhookClient({
+      id: process.env.ROLE_EDIT_WEBHOOK_ID,
+      token: process.env.ROLE_EDIT_WEBHOOK_TOKEN,
+    });
+
+    const editLog = await guild
+      .fetchAuditLogs({
+        limit: 1,
+        type: AuditLogEvent.RoleUpdate,
+      })
+      .then((audit) => audit.entries.first());
+
+    const embed = new EmbedBuilder()
+      .setColor(guild.members.me.displayHexColor)
+      .setTimestamp(Date.now())
+      .setFooter({
+        text: client.user.username,
+        iconURL: client.user.displayAvatarURL({ dynamic: true }),
+      })
+      .setAuthor({
+        name: '⚒️ Role Edited',
+      })
+      .setDescription(
+        `${oldRole} role was ${bold('edited')} by ${editLog.executor} at ${time(
+          Math.floor(Date.now() / 1000),
+          TimestampStyles.RelativeTime,
+        )}.`,
+      )
+      .setFields([
+        {
+          name: 'Before',
+          value: oldRole.name,
+        },
+        {
+          name: 'After',
+          value: newRole.name,
+        },
+      ]);
+
+    await RoleLogger.send({ embeds: [embed] }).catch((err) =>
+      console.error(err),
+    );
+  },
+};
