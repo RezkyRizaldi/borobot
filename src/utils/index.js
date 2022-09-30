0f65e98990578e861e3509244f5248cc6eaa20e8
RezkyRizaldi <rezkyrizaldi30@gmail.com>
2022-10-01 06:56:20 +0700

    Improve role slash command

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,11 +1,13 @@
 const applyActivity = require('./applyActivity');
 const applyAFKTimeout = require('./applyAFKTimeout');
+const applyComparison = require('./applyComparison');
 const applyHexColor = require('./applyHexColor');
 const applyMessageType = require('./applyMessageType');
 const applyNSFWLevel = require('./applyNSFWLevel');
+const applyOrdinal = require('./applyOrdinal');
 const applyPresence = require('./applyPresence');
 const applyRepeatMode = require('./applyRepeatMode');
-const serverMute = require('./serverMute');
+const applySpacesBetweenPascalCase = require('./applySpacesBetweenPascalCase');
 const applyText = require('./applyText');
 const applyTier = require('./applyTier');
 const applyVerificationLevel = require('./applyVerificationLevel');
@@ -16,17 +18,20 @@ const getMessageType = require('./getMessageType');
 const groupMessageByAuthor = require('./groupMessageByAuthor');
 const groupMessageByType = require('./groupMessageByType');
 const isValidURL = require('./isValidURL');
+const serverMute = require('./serverMute');
 const truncate = require('./truncate');
 
 module.exports = {
   applyActivity,
   applyAFKTimeout,
+  applyComparison,
   applyHexColor,
   applyMessageType,
   applyNSFWLevel,
+  applyOrdinal,
   applyPresence,
   applyRepeatMode,
-  serverMute,
+  applySpacesBetweenPascalCase,
   applyText,
   applyTier,
   applyVerificationLevel,
@@ -37,5 +42,6 @@ module.exports = {
   groupMessageByAuthor,
   groupMessageByType,
   isValidURL,
+  serverMute,
   truncate,
 };
