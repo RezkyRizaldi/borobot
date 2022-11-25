c8ad100b41526cfe5d708abb3fba184d8db4c6f5
RezkyRizaldi <rezkyrizaldi30@gmail.com>
2022-11-25 09:47:17 +0700

    Sorting commands

--- src/functions/handlers/handleCommands.js
+++ src/functions/handlers/handleCommands.js
@@ -59,7 +59,7 @@ module.exports = (client) => {
           process.env.CLIENT_ID,
           process.env.GUILD_ID,
         ),
-        { body: commandArray },
+        { body: commandArray.sort((a, b) => a.name.localeCompare(b.name)) },
       )
       .then(() =>
         console.log('Successfully reloaded application (/) commands.'),
