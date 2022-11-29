710ca574056b29ce9a07b3b2072a9dec97e38ded
RezkyRizaldi <rezkyrizaldi30@gmail.com>
2022-11-29 17:14:31 +0700

    Fix vulnerability

--- src/utils/isValidURL.js
+++ src/utils/isValidURL.js
@@ -7,28 +7,29 @@
 module.exports = (str, site) => {
   if (site) {
     return {
-      facebook: /(?:(?:http|https):\/\/)?(?:www.)?facebook.com\/?gi/.test(str),
+      facebook: /(?:(?:http|https):\/\/)?(?:www\.)?facebook\.com\/?gi/.test(
+        str,
+      ),
       instagram:
-        /(?:(?:http|https):\/\/)?(?:www.)?(?:instagram.com|instagr.am|instagr.com)\/(\w+)\/?/.test(
+        /(?:(?:http|https):\/\/)?(?:www\.)?(?:instagram\.com|instagr\.am|instagr\.com)\/(\w+)\/?/.test(
           str,
         ),
       spotify: /^(spotify:|https:\/\/(open|play)\.spotify\.com\/)/.test(str),
-      tiktok: /(?:(?:http|https):\/\/)?(?:vt.)?(?:tiktok.com)\/(\w+)\/?/.test(
+      tiktok: /(?:(?:http|https):\/\/)?(?:vt\.)?(?:tiktok\.com)\/(\w+)\/?/.test(
         str,
       ),
       twitter:
-        /(?:(?:http|https):\/\/)?(?:www.|mobile.)?(?:twitter.com)\/(\w+)\/(status)\/(\d+)?/.test(
+        /(?:(?:http|https):\/\/)?(?:www\.|mobile\.)?(?:twitter\.com)\/(\w+)\/(status)\/(\d+)?/.test(
           str,
         ),
       youtube:
-        /^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube(-nocookie)?\.com|youtu.be))(\/(?:[\w-]+\?v=|embed|shorts|\/|v\/)?)([\w-]+)(\S+)?$/.test(
+        /^((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube(-nocookie)?\.com|youtu\.be))(\/(?:[\w-]+\?v=|embed|shorts|\/|v\/)?)([\w-]+)(\S+)?$/.test(
           str,
         ),
     }[site];
   }
 
-  return new RegExp(
-    '^(https?:\\/\\/)?((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|((\\d{1,3}\\.){3}\\d{1,3}))(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*(\\?[;&a-z\\d%_.~+=-]*)?(\\#[-a-z\\d_]*)?$',
-    'i',
-  ).test(str);
+  return /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=+$,\w]+@)?[A-Za-z0-9.-]+(:[0-9]+)?|(?:www.|[-;:&=+$,\w]+@)[A-Za-z0-9.-]+)((?:\/[+~%/.\w-_]*)?\??(?:[-+=&;%@.\w_]*)#?(?:[\w]*))?)/.test(
+    str,
+  );
 };
